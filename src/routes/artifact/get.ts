e4ad7b310774e6bfa65ed5e017ad64a19669828f
Oleg Kamlowski <kamlowski.oleg@gmail.com>
2022-03-25 11:25:48 +0100

    feat, add all artifacts route

---
+++ src/routes/artifact/get.ts
@@ -0,0 +1,21 @@
+import container from 'container';
+
+/**
+ * User: Oleg Kamlowski <oleg.kamlowski@thomann.de>
+ * Date: 30.01.2019
+ * Time: 17:39
+ */
+export default container(async ({artifacts}) => {
+    const data = artifacts.all()
+        .map((artifact) => ({
+            id: artifact._id,
+            size: artifact.size,
+            context: artifact.context,
+        }))
+    ;
+
+    return {
+        status: 'OK',
+        data,
+    };
+});
