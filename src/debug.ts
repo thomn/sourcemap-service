40632f111c3f4c3957d74fef412bd73176c9c2d1
Oleg Kamlowski <kamlowski.oleg@gmail.com>
2022-03-29 16:00:25 +0200

    feat, sentry add attachments

--- src/debug.ts
+++ src/debug.ts
@@ -1,4 +1,7 @@
+import {request} from 'https';
 import * as Sentry from '@sentry/node';
+import type {NodeClient} from '@sentry/node'
+import FormData from 'form-data';
 import config from '#/config';
 
 config().then(({SENTRY_DSN}) => {
@@ -6,6 +9,7 @@ config().then(({SENTRY_DSN}) => {
         dsn: null,
         enabled: false,
         maxBreadcrumbs: 0,
+        debug: true,
     };
 
     if (SENTRY_DSN) {
@@ -18,11 +22,75 @@ config().then(({SENTRY_DSN}) => {
 
 /**
  *
- * @param exception
+ * @param attachments
+ * @param event
+ * @param client
+ */
+const attachAsJson = async (attachments: object[], event, client: NodeClient) => {
+    const dsn = client.getDsn();
+    if (!dsn) {
+        return;
+    }
+
+    const {host, projectId, port, user} = dsn;
+    const eventId = event.event_id;
+
+    const formData = new FormData();
+    for (const attachment of attachments) {
+        formData.append(
+            'application/json',
+            JSON.stringify(attachment),
+            'attachment',
+        );
+    }
+
+    const req = request({
+        method: 'POST',
+        host,
+        port,
+        protocol: 'https:',
+        path: `/api/${projectId}/events/${eventId}/attachments/?`
+            + `sentry_key=${user}&sentry_version=7&sentry_client=custom-javascript`,
+        headers: formData.getHeaders(),
+    });
+    req.on('error', console.error);
+    formData.pipe(req);
+    req.end();
+};
+
+/**
+ *
+ * @param e
  * @param context
  */
-const capture = (exception: any, context?: any) => {
-    Sentry.captureException(exception, context);
+const capture = async (e: any, context?: any): Promise<void> => {
+    const promise = new Promise(
+        (resolve) => {
+            Sentry.withScope((scope) => {
+                if (context.attachments && context.attachments.length) {
+                    scope.addEventProcessor(async (event) => {
+                        try {
+                            await attachAsJson(
+                                context.attachments,
+                                event,
+                                Sentry.getCurrentHub().getClient(),
+                            );
+                        } catch (ex) {
+                            console.error(ex);
+                        }
+
+                        return event;
+                    });
+                }
+
+                Sentry.captureException(e);
+                resolve(null);
+            });
+        },
+    );
+
+    await promise;
+    await Sentry.flush();
 };
 
 /**
