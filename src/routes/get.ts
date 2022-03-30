1ebbc0830fef67728ac10f27ec537497b2b1d65e
Oleg Kamlowski <kamlowski.oleg@gmail.com>
2022-03-30 11:37:56 +0200

    fix, add root route

---
+++ src/routes/get.ts
@@ -0,0 +1,10 @@
+import container from '#/container';
+
+/**
+ * User: Oleg Kamlowski <oleg.kamlowski@thomann.de>
+ * Date: 30.03.2022
+ * Time: 11:31
+ */
+export default container(() => ({
+    status: 'OK',
+}));
